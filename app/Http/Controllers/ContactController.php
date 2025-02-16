bd5818ab68c782e6b40a2a4633a65e565dbe1c05
tomy.dev <xxx.d1231@gmail.com>
2025-02-17 00:01:49 +0900

    コメント変更

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,7 +24,6 @@ class ContactController extends Controller
         ]);
 
         // これ以降の行は入力エラーがなかった場合のみ実行されます
-        // 登録処理(実際はメール送信などを行う)
         Log::debug($validated['name'] . 'さんよりお問い合わせがありました');
         return to_route('contact.complete');
     }
