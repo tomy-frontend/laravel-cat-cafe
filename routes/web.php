0dba09e5c1f9714b54952ba8df1387e66a4ecf0f
tomy.dev <xxx.d1231@gmail.com>
2025-02-16 11:08:23 +0900

    フォーム送信のバリデーションまで

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 // ルートディレクトリ
 Route::view('/', 'index');
+
+// contactページ
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'sendMail']);
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
