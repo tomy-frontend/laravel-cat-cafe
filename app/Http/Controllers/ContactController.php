2a50b9f652f1016f332597c9012aee094ee8ee7c
tomy.dev <xxx.d1231@gmail.com>
2025-02-18 10:34:05 +0900

    メール処理関数部分修正

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,6 @@ class ContactController extends Controller
     {
         $validated = $request->validated();
 
-        // これ以降の行は入力エラーがなかった場合のみ実行されます
         Log::debug($validated["name"] . "さんよりお問い合わせがありました");
         return to_route("contact.complete");
     }
