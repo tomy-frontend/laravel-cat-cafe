20d61de98bb6006126b65e8a8c129c1d69c9c93b
tomy.dev <xxx.d1231@gmail.com>
2025-02-23 23:53:36 +0900

    モデルとルート結合

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -51,9 +51,8 @@ class AdminBlogController extends Controller
     }
 
     // 指定したIDのブログ編集画面
-    public function edit(string $id)
+    public function edit(Blog $blog)
     {
-        $blog = Blog::findOrFail($id);
         return view("admin.blogs.edit", ['blog' => $blog]);
     }
 
