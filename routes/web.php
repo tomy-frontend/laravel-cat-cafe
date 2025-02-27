291bc89a3f7318b420dffe03a9d3c45ce0ebeec4
tomy.dev <xxx.d1231@gmail.com>
2025-02-27 11:07:42 +0900

    Routeグループのまとめまで

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,29 @@ Route::get("/contact/complete", [ContactController::class, "complete"])->name(
     "contact.complete"
 );
 
-// ブログ
-Route::get("/admin/blogs", [AdminBlogController::class, "index"])->name(
-    "admin.blogs.index"
-)->middleware('auth');
-Route::get("/admin/blogs/create", [AdminBlogController::class, "create"])->name(
-    "admin.blogs.create"
-);
-Route::post("/admin/blogs", [AdminBlogController::class, "store"])->name(
-    "admin.blogs.store"
-);
-Route::get("/admin/blogs/{blog}", [AdminBlogController::class, "edit"])->name(
-    "admin.blogs.edit"
-)->middleware('auth');
-Route::put("/admin/blogs/{blog}", [AdminBlogController::class, "update"])->name(
-    "admin.blogs.update"
-);
-Route::delete("/admin/blogs/{blog}", [AdminBlogController::class, "destroy"])->name(
-    "admin.blogs.destroy"
-);
+// 管理画面
+Route::prefix('/admin')
+    ->name('admin.')
+    ->group(function () {
+        // ログイン時のみアクセス可能なルート
+        Route::middleware('auth')
+            ->group(function () {
+                // ブログ
+                Route::resource('/blogs', AdminBlogController::class)->except('show');
 
-// ユーザー管理
-Route::get("/admin/users/create", [UserController::class, "create"])->name("admin.users.create");
-Route::post("/admin/users", [UserController::class, "store"])->name('admin.users.store');
+                // ユーザー管理
+                Route::get("/users/create", [UserController::class, "create"])->name("users.create");
+                Route::post("/users", [UserController::class, "store"])->name('users.store');
 
-// 認証
-Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login')->middleware('guest');
-Route::post('/admin/login', [AuthController::class, 'login']);
-Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+                // ログアウト
+                Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+            });
+
+        // 未ログイン時のみアクセス可能なルート
+        // 認証
+        Route::middleware('guest')
+            ->group(function () {
+                Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+                Route::post('/login', [AuthController::class, 'login']);
+            });
+    });
